c39a1504661a61f5a1667f7d984a5963912bbfed
ldex <laurent.duveau+git@gmail.com>
2022-01-24 16:39:54 -0500

    Fin du workshop

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Observable, EMPTY, combineLatest, Subscription } from 'rxjs';
-import { tap, catchError, startWith, count, flatMap, map, debounceTime, filter } from 'rxjs/operators';
+import {
+  tap,
+  catchError,
+  startWith,
+  count,
+  flatMap,
+  map,
+  debounceTime,
+  filter,
+  distinctUntilChanged,
+} from 'rxjs/operators';
 
 import { Product } from '../product.interface';
 import { ProductService } from '../product.service';
 import { FavouriteService } from '../favourite.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
-
+export class ProductListComponent implements OnInit, OnDestroy {
   title: string = 'Products';
   selectedProduct: Product;
   products$: Observable<Product[]>;
+  productsNumber$: Observable<number>;
+  filteredProducts$: Observable<Product[]>;
+  filter$: Observable<string>;
+  filtered$: Observable<boolean>;
+
+  favouriteAdded: Product;
+  sub: Subscription = new Subscription();
+
   errorMessage;
+
+  filter: FormControl = new FormControl('');
 
   // Pagination
   pageSize = 5;
@@ -52,20 +72,71 @@ export class ProductListComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private favouriteService: FavouriteService,
-    private router: Router) {
+    private router: Router
+  ) {}
+
+  private firstPage(): void {
+    this.start = 0;
+    this.end = this.pageSize;
+    this.currentPage = 1;
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
   }
 
   ngOnInit(): void {
+    this.sub.add(this.favouriteService.favouriteAdded$
+                          .pipe(
+                            tap(console.log)
+                          )
+                          .subscribe(
+                            product => this.favouriteAdded = product
+                          ));
+
+
     // Self url navigation will refresh the page ('Refresh List' button)
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
-    this.products$ = this
-                      .productService
-                      .products$;
+
+    this.products$ = this.productService.products$;
+
+    this.filter$ = this.filter
+                      .valueChanges
+                      .pipe(
+                        map(text => text.trim()),
+                        filter(text => text == "" || text.length > 2),
+                        debounceTime(500),
+                        distinctUntilChanged(),
+                        startWith(''),
+                        tap(text => {
+                         // console.warn(text);
+                          this.firstPage();
+                        })
+                      );
+
+    this.filtered$ = this.filter$
+                          .pipe(
+                            map(text => text.length > 0)
+                          )
+
+    this.filteredProducts$ = combineLatest([this.products$, this.filter$])
+                      .pipe(
+                        map(([products, filterString]) =>
+                          products.filter(product =>
+                            product.name.toLowerCase().includes(filterString.toLowerCase())
+                          )
+                        )
+                      )
+
+      this.productsNumber$ = this.filteredProducts$.pipe(
+        map((products) => products.length),
+        startWith(0)
+      );
   }
 
   refresh() {
     this.productService.initProducts();
     this.router.navigateByUrl('/products'); // Self route navigation
-  }  
+  }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,18 +3,22 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, delay, shareReplay, tap } from 'rxjs/operators';
 import { Product } from './product.interface';
+import { LoadingService } from '../services/loading.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'https://storerestservice.azurewebsites.net/api/products/';
+  private baseUrl = 'https://storerestservice1354.azurewebsites.net/api/products/';
   products$: Observable<Product[]>;
 
-  constructor(private http: HttpClient) { 
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService) {
+
     this.initProducts();
-  }
+ }
 
   initProducts() {
     let url:string = this.baseUrl + `?$orderby=ModifiedDate%20desc`;
@@ -24,9 +28,11 @@ export class ProductService {
                       .get<Product[]>(url)
                       .pipe(
                         delay(1500),
-                        tap(console.table),
+                      //  tap(console.table),
                         shareReplay()
                       );
+
+    this.loadingService.showLoaderUntilCompleted(this.products$);
   }
 
   insertProduct(newProduct: Product): Observable<Product> {
@@ -34,6 +40,6 @@ export class ProductService {
   }
 
   deleteProduct(id: number): Observable<any> {
-    return this.http.delete(this.baseUrl + id);           
+    return this.http.delete(this.baseUrl + id);
   }
 }
